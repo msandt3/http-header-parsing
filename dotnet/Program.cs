aa06e877be179797fa4b16b2591aebcfddbb4b9c
Mike Sandt <michael.sandt@gmail.com>
2017-01-17 07:37:33 -0800

    Write output to files

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -11,11 +11,14 @@ namespace MehdiMe.HttpHeaderParsingRepro
 
 		// Set the URL to test here
 	    private const string Url = "https://sdr.qasalesloft.com/api/users/authenticate";
+        private const string filename = "c:\\Users\\User\\src\\qa-test.txt";
 
         static void Main(string[] args)
         {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             Console.WriteLine("Will be making {0} POST requests to {1}. Press Enter to start.", NbIterations, Url);
+            StreamWriter file = new StreamWriter(filename);
+            file.Write("Will be making {0} POST requests to {1}. Press Enter to start.", NbIterations, Url);
 
             var nbProtocolViolations = 0;
             var nbOtherErrors = 0;
@@ -39,15 +42,18 @@ namespace MehdiMe.HttpHeaderParsingRepro
                     {
                         nbProtocolViolations++;
                         Console.Write(e);
+                        file.WriteLine(e);
                     }
                     else if (e.Response is HttpWebResponse && ((HttpWebResponse) e.Response).StatusCode >= HttpStatusCode.BadRequest)
                     {
 						Console.Write("Success - " + ((HttpWebResponse)e.Response).StatusCode);
+                        file.WriteLine("Success - " + ((HttpWebResponse)e.Response).StatusCode);
                     }
                     else
                     {
                         nbOtherErrors++;
                         Console.Write(e);
+                        file.WriteLine(e);
                     }
                 }
                 catch (Exception e)
@@ -62,7 +68,10 @@ namespace MehdiMe.HttpHeaderParsingRepro
             Console.WriteLine(
                 "The end. Number of tries: {0}. Number of protocol violations: {1}. Number of other errors: {2}. Press Enter to exit.",
                 NbIterations, nbProtocolViolations, nbOtherErrors);
+            file.WriteLine("The end. Number of tries: {0}. Number of protocol violations: {1}. Number of other errors: {2}. Press Enter to exit.",
+                NbIterations, nbProtocolViolations, nbOtherErrors);
             Console.ReadLine();
+            file.Close();
         }
 
         private static string ReadEmbeddedResource(string resourceName)
